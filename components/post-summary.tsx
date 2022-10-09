2a7a891cc8b3214ab8447f4da188c793e7a4c9b1
Simon Hessel <simon.hessel@kreios.lu>
2022-10-09 22:49:55 +0200

    Updated packages

--- components/post-summary.tsx
+++ components/post-summary.tsx
@@ -82,51 +82,55 @@ export function PostSummary({
             </Link>
           )}
           <div className="ml-auto flex gap-6">
-            <Tooltip.Root delayDuration={300}>
-              <Tooltip.Trigger
-                asChild
-                onClick={(event) => {
-                  event.preventDefault()
-                }}
-                onMouseDown={(event) => {
-                  event.preventDefault()
-                }}
-              >
-                <div className="inline-flex items-center gap-1.5">
-                  {isLikedByCurrentUser ? (
-                    <HeartFilledIcon className="w-4 h-4 text-red" />
-                  ) : (
-                    <HeartIcon className="w-4 h-4 text-red" />
+            <Tooltip.Provider>
+              <Tooltip.Root delayDuration={300}>
+                <Tooltip.Trigger
+                  asChild
+                  onClick={(event) => {
+                    event.preventDefault()
+                  }}
+                  onMouseDown={(event) => {
+                    event.preventDefault()
+                  }}
+                >
+                  <div className="inline-flex items-center gap-1.5">
+                    {isLikedByCurrentUser ? (
+                      <HeartFilledIcon className="w-4 h-4 text-red" />
+                    ) : (
+                      <HeartIcon className="w-4 h-4 text-red" />
+                    )}
+                    <span className="text-sm font-semibold tabular-nums">
+                      {likeCount}
+                    </span>
+                  </div>
+                </Tooltip.Trigger>
+                <Tooltip.Content
+                  side="bottom"
+                  sideOffset={4}
+                  className={classNames(
+                    'max-w-[260px] px-3 py-1.5 rounded shadow-lg bg-secondary-inverse text-secondary-inverse sm:max-w-sm',
+                    likeCount === 0 && 'hidden'
                   )}
-                  <span className="text-sm font-semibold tabular-nums">
-                    {likeCount}
-                  </span>
-                </div>
-              </Tooltip.Trigger>
-              <Tooltip.Content
-                side="bottom"
-                sideOffset={4}
-                className={classNames(
-                  'max-w-[260px] px-3 py-1.5 rounded shadow-lg bg-secondary-inverse text-secondary-inverse sm:max-w-sm',
-                  likeCount === 0 && 'hidden'
-                )}
-              >
-                <p className="text-sm">
-                  {post.likedBy
-                    .slice(0, MAX_LIKED_BY_SHOWN)
-                    .map((item) =>
-                      item.user.id === session!.user.id ? 'You' : item.user.name
-                    )
-                    .join(', ')}
-                  {likeCount > MAX_LIKED_BY_SHOWN &&
-                    ` and ${likeCount - MAX_LIKED_BY_SHOWN} more`}
-                </p>
-                <Tooltip.Arrow
-                  offset={22}
-                  className="fill-gray-800 dark:fill-gray-50"
-                />
-              </Tooltip.Content>
-            </Tooltip.Root>
+                >
+                  <p className="text-sm">
+                    {post.likedBy
+                      .slice(0, MAX_LIKED_BY_SHOWN)
+                      .map((item) =>
+                        item.user.id === session!.user.id
+                          ? 'You'
+                          : item.user.name
+                      )
+                      .join(', ')}
+                    {likeCount > MAX_LIKED_BY_SHOWN &&
+                      ` and ${likeCount - MAX_LIKED_BY_SHOWN} more`}
+                  </p>
+                  <Tooltip.Arrow
+                    offset={22}
+                    className="fill-gray-800 dark:fill-gray-50"
+                  />
+                </Tooltip.Content>
+              </Tooltip.Root>
+            </Tooltip.Provider>
 
             <div className="inline-flex items-center gap-1.5">
               <MessageIcon className="w-4 h-4 text-secondary" />
